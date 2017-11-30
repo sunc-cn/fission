4ed66db2b1573923c4e8dc188c4456c59b1f4056
Puneet Gupta <puneetguptanitj@users.noreply.github.com>
2017-11-30 16:26:13 +0100

    added support for kafka message queue
    (cherry picked from commit cfcd9a5)

--- types.go
+++ types.go
@@ -136,8 +136,9 @@ const (
 )
 
 const (
-	MessageQueueTypeNats = fv1.MessageQueueTypeNats
-	MessageQueueTypeASQ  = fv1.MessageQueueTypeASQ
+	MessageQueueTypeNats  = fv1.MessageQueueTypeNats
+	MessageQueueTypeASQ   = fv1.MessageQueueTypeASQ
+	MessageQueueTypeKafka = fv1.MessageQueueTypeKafka
 )
 
 const (
